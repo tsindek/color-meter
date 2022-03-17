1ba3a46ab256c2ec39c1dacf4216090269d1982e
Tobias Sindek <tobias.sindek@gmail.com>
2022-03-17 14:17:40 +0100

    feat: add random color feature
    color data gets fetched from API

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-const redSlider = document.querySelector("#redSlider");
-const greenSlider = document.querySelector("#greenSlider");
-const blueSlider = document.querySelector("#blueSlider");
-const main = document.querySelector("main");
+const body = document.querySelector("body");
 const header = document.querySelector("header");
 const headerWrapper = document.querySelector(".header__wrapper");
 const colorValue = document.querySelector(".colorValue");
+const randomButton = document.querySelector(".random-button");
 
-function calculateBackgroundColor(redSlider, greenSlider, blueSlider) {
-  main.style.backgroundColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
-  headerWrapper.style.backgroundColor = `rgba(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value}, 0.5)`;
-  colorValue.textContent = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
+function calculateBackgroundColor(red, green, blue) {
+  body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  colorValue.textContent = `rgb(${red}, ${green}, ${blue})`;
 }
 
-header.addEventListener("change", (e) => {
+header.addEventListener("input", (e) => {
+  const redSlider = document.querySelector("#red-slider").value;
+  const greenSlider = document.querySelector("#green-slider").value;
+  const blueSlider = document.querySelector("#blue-slider").value;
+
   calculateBackgroundColor(redSlider, greenSlider, blueSlider);
+});
+
+function getRandomColor() {
+  fetch("https://dummy-apis.netlify.app/api/color")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((colorData) => {
+      const red = colorData.rgb.r;
+      const green = colorData.rgb.g;
+      const blue = colorData.rgb.b;
+
+      calculateBackgroundColor(red, green, blue);
+    });
+}
+
+randomButton.addEventListener("click", () => {
+  getRandomColor();
 });
